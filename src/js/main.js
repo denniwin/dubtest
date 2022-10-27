52de94a03d23e469fb3adb737b0be33298205acb
Den <denis032014@bk.ru>
2022-10-27 17:56:46 +0300

    addProject

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    var typed = new Typed('.hero__title_step', {
+        strings: ["планам развития", "адаптации новичков", "карьерным траекториям"],
+        typeSpeed: 70,
+        backSpeed: 55,
+        loop: true,
+    });
+
+
+});    
