316fbcb6da81ecaa25d281ac764825667ce41208
Den <denis032014@bk.ru>
2022-10-28 22:01:41 +0300

    addAllSection

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
 
-    var typed = new Typed('.hero__title_step', {
+    var typed = new Typed('.section__title_step', {
         strings: ["планам развития", "адаптации новичков", "карьерным траекториям"],
         typeSpeed: 70,
         backSpeed: 55,
         loop: true,
     });
 
+    $('.menu-trigger').on('click', function() {
+        $(this).toggleClass('active');
+        return false;
+      });
 
 });    
